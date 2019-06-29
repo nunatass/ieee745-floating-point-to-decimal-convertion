5eff412da24bd03e36abbe8eee31b7fc3aa30b44
nunatass <nunatass@gmail.com>
2019-06-29 04:05:26 +0100

    commit iee_745.c file
    for now it just convert ieee745  (hex) number to decimal number, but some error if the number is to big or to small.

---
+++ ieee_745.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define GETSIGNAL 0x80000000
+#define GETEXPONENT 0x7f800000
+#define GeTMANTISSA 0x007fffff
+
+int main(int argc, const char *argv[]) {
+
+
+    int hexNumber =  strtol(argv[1], NULL, 16);
+    float mantissa = 1;
+    unsigned int sign = 0; 
+    unsigned int exponent = 0;
+    float decNumber = 0;
+
+    //getting the sign
+    sign =  hexNumber & GETSIGNAL;
+    sign = sign >> 31;
+  
+    // getting the exponent
+    exponent =  hexNumber & GETEXPONENT;
+    exponent =  exponent << 1;
+    exponent = exponent >> 24;
+    exponent =  exponent -127;
+   
+    //get the abgstract mantissa
+    int mantissaAbstract = hexNumber & GeTMANTISSA;
+
+    // aux to get the active or not active bit
+    int getBitFromMantissaAbstract  =  0x00400000;
+
+    for(int i = 1; i<=23; i++) {
+      int bit = mantissaAbstract & getBitFromMantissaAbstract;
+      bit =  bit >> (23-i);
+      getBitFromMantissaAbstract =  getBitFromMantissaAbstract >> 1;
+      mantissa = mantissa + bit*pow(2, (-1*i)); 
+     
+   }
+
+   // condição ? codigoUm : codigoDois;
+
+    decNumber = pow(2, exponent) * mantissa;
+    decNumber = (sign == 0) ? decNumber: (-1*decNumber);
+
+
+    //printf("%ld\n", strtol(argv[1], NULL, 16));
+  
+  return 0;
+}
